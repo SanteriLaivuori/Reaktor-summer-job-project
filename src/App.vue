846f4440256edb18b8676598b12c1b112ab5ddc6
SanteriLaivuori <santeri.laivuori@gmail.com>
2019-05-14 10:42:32 +0300

    Completely changed project structure

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="app">
+
+  </div>
+</template>
+
+<script>
+// import emission from "@/assets/emission.csv";
+import axios from "axios";
+
+export default {
+  name: "app",
+  data: () => ({}),
+  async created() {
+    const jsonArray = await axios
+      .get(
+        "https://cors-anywhere.herokuapp.com/http://snowcoding.com/reaktor/emission.csv"
+      )
+      .then(result => result.data);
+    console.log(jsonArray);
+  }
+};
+</script>
+
+<style>
+.app {
+  background-image: url("https://fw-d7-freedomworks-org.s3.amazonaws.com/field/image/70250513-factory-wallpapers.jpg");
+  background-position: center;
+  background-size: cover;
+
+}
+</style>
